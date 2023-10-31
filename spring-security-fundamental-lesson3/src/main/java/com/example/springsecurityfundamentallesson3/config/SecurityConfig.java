7c0f4784efac6787daec421c22b7c64285d0e909
hmd <79966404+hmdresearch@users.noreply.github.com>
2023-10-31 10:51:07 +0700

    Update SecurityConfig.java

--- spring-security-fundamental-lesson3/src/main/java/com/example/springsecurityfundamentallesson3/config/SecurityConfig.java
+++ spring-security-fundamental-lesson3/src/main/java/com/example/springsecurityfundamentallesson3/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 
+@EnableWebSecurity
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig {
